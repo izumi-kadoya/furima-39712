7e8a62ba298d07bdffca8607f60d5ac2e991803c
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-24 16:41:12 +0900

    コントローラー、ビューの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,11 +23,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    unless user_signed_in? && current_user.id == @item.user_id 
+      redirect_to root_path
+    end
   end
 
   def update
-    item = Item.find(params[:id])
-     if     item.update(item_params)
+     if @item.update(item_params)
        redirect_to item_path
      else
        render :edit, status: :unprocessable_entity
