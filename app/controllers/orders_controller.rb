0d2038510d9c63e3c9177733408306d8b9cf5088
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-25 17:30:40 +0900

    購入アクションの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,28 @@
 class OrdersController < ApplicationController
-
+  before_action :set_item, only: [:index, :create]
   def index
-    @item = Item.find(params[:item_id])
   end
 
+  def new
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.save
+      redirect_to root_path 
+    else
+      render :new 
+    end
+  end
+
+private
+def set_item
+  @item = Item.find(params[:item_id])
+end
+
+def purchase_params
+ params.require(:purchase_address).permit(:postcode, :prefecture, :city, :street, :building, :tel).merge(user_id: current_user.id,item_id: @item.id)
+end
 
 end
