6057d0b2095fd1ac271ddbb22ff02a18134ab65c
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-25 10:47:26 +0900

    コントローラーの修正
    本人のみに操作を許可

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -23,8 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    return if current_user.id == @item.user_id
-    redirect_to root_path
   end
 
   def update
@@ -38,7 +37,8 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     redirect_to root_path 
-   end  
+  end  
+
   private
 
   def item_params
@@ -48,5 +48,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def check_user
+      redirect_to root_path unless current_user.id == @item.user_id
   end
 end
