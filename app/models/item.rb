681ebeaf61ce844e626e619973f963f0ddc38c3b
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-26 14:10:52 +0900

    Revert "Merge pull request #19 from izumi-kadoya/商品購入機能#10--step1"
    This reverts commit 990622df3dae1f41571535f2347e262eb3c0f650, reversing
    changes made to 05d4346e2c1c00d7b734de95684d5988b43e67d6.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_one :purchase
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
