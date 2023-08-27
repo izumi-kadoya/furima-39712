aded0f136b2cc5cbc5d7b4c39587db9eb96cc478
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-27 18:19:04 +0900

    gon導入まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,17 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_address = PurchaseAddress.new
   end
 
   def create
+    if @item.purchase.present?
+      redirect_to root_path
+    else
     @purchase_address = PurchaseAddress.new(purchase_params)
-   if @item.purchase.present?
-    redirect_to root_path
-   else
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path 
     else
@@ -25,7 +27,15 @@ def set_item
 end
 
 def purchase_params
- params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :street, :building, :tel).merge(user_id: current_user.id,item_id: @item.id)
+ params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :street, :building, :tel).merge(user_id: current_user.id,item_id: @item.id,token: params[:token])
 end
 
+def pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  Payjp::Charge.create(
+    amount: @item.price,  # 商品の値段
+    card: purchase_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
+end
 end
