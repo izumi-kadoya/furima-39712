457232b0e46d2cdbc319b57e6b47cd15ecb94e39
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-28 14:53:50 +0900

    Revert "テストコード記述の準備"
    This reverts commit 9398493e53c7f8eab6008b52cf1a7f59abd17030.

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_address do
     token {"tok_abcdefghijk00000000000000000"}
-    postcode { "000-0000"}
+    postcode { 000-0000}
     prefecture_id { 2 }
     city {Faker::Address.city}
     street {Faker::Address.street_name }
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -2,27 +2,26 @@ require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
   before do
-    @order = FactoryBot.build(:purchase_address)
+    @order = FactoryBot.build(:order)
   end
 
   context '内容に問題ない場合' do
-    it "tokenと住所と電話番号があれば保存ができること" do
+    it "priceとtokenがあれば保存ができること" do
       expect(@order).to be_valid
     end
   end
 
   context '内容に問題がある場合' do
+    it "priceが空では保存ができないこと" do
+      @order.price = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Price can't be blank")
+    end
 
     it "tokenが空では登録できないこと" do
       @order.token = nil
       @order.valid?
       expect(@order.errors.full_messages).to include("Token can't be blank")
     end
-
-# 住所などに問題ある場合の記述を書く
-
-
-
-
   end
 end
