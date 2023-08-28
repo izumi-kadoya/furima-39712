44de8608cfc3cd1d3ff8efad3f7f78aacf9abeea
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-28 10:28:23 +0900

    単体テストコードの記述

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :purchase_address do
     token {"tok_abcdefghijk00000000000000000"}
-    postcode { "000-0000"}
+    postcode { "123-4567"}
     prefecture_id { 2 }
     city {Faker::Address.city}
     street {Faker::Address.street_name }
+    building {"building123"}
     tel{ Faker::Number.leading_zero_number(digits: [10, 11].sample) }
   end
 end
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe PurchaseAddress, type: :model do
     it "tokenと住所と電話番号があれば保存ができること" do
       expect(@order).to be_valid
     end
+    it "建物名が空でも保存ができること" do
+     @order.building = ""
+     expect(@order).to be_valid
+    end
   end
 
   context '内容に問題がある場合' do
@@ -18,10 +22,62 @@ RSpec.describe PurchaseAddress, type: :model do
       @order.valid?
       expect(@order.errors.full_messages).to include("Token can't be blank")
     end
+    it "郵便番号が空では登録できないこと" do
+      @order.postcode = ""
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Postcode can't be blank", "Postcode is invalid. Enter it as follows (e.g. 123-4567)")
+    end
 
-# 住所などに問題ある場合の記述を書く
-
-
+    it "郵便番号にハイフンがない状態では登録できないこと" do
+      @order.postcode = "1234567"
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Postcode is invalid. Enter it as follows (e.g. 123-4567)")
+    end
+    it "郵便番号が全角入力の状態では登録できないこと" do
+      @order.postcode= "１２３ー４５６７"
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Postcode is invalid. Enter it as follows (e.g. 123-4567)")
+      end
+    it "都道府県が空では登録できないこと" do
+      @order.prefecture_id = ''
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it "都道府県の「カテゴリー」が「---」では登録できないこと" do
+      @order.prefecture_id = 1
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it "市区町村が空では登録できないこと" do
+      @order.city = ""
+      @order.valid?
+      expect(@order.errors.full_messages).to include("City can't be blank")
+    end
+    it "番地が空では登録できないこと" do
+      @order.street = ""
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Street can't be blank")
+    end
+    it "電話番号が空では登録できないこと" do
+      @order.tel = ""
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Tel can't be blank")
+    end
+    it "電話番号が全角では登録できないこと" do
+      @order.tel = "０９０１２３４５６７８"
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Tel is invalid")
+    end
+    it "電話番号が９桁以下では登録できないこと" do
+      @order.tel = "12345678"
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Tel is invalid")
+    end  
+    it "電話番号が１２桁以上では登録できないこと" do
+      @order.tel = "123456789012"
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Tel is invalid")
+    end
 
 
   end
