319dc17aa211780d351090901ab85cf2cb67ce26
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-09-05 15:07:53 +0900

    itemprice.js

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("turbo:load", () => {
   const priceInput = document.getElementById("item-price");
   if (priceInput) {
     priceInput.addEventListener("input", () => {
